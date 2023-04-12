d580c239cbd9b851fde924b56196ac2c661406f0
Danilo "Aranha <daniloprado7@gmail.com>
2023-04-12 13:22:46 -0300

    Adicionado moeda, e base de coletáveis

---
+++ Assets/Scripts/Collectables/CollectableBase.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class CollectableBase : MonoBehaviour
+{
+    public float timeToHide;
+    public GameObject model;
+
+    private string _playerTag = "Player";
+    private Collider _collider;
+
+    private void OnValidate()
+    {
+        _collider = GetComponent<Collider>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(_playerTag))
+        {
+            CollectItem();
+        }
+    }
+    private void CollectItem()
+    {
+        _collider.enabled = false;
+        Destroy(gameObject);
+        OnCollectItem();
+    }
+    private void OnCollectItem()
+    {
+
+    }
+}
